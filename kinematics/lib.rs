3a017877076eba9ffc4dd462f75ac4e1aeb320a9
Luke <luke.rieff@gmail.com>
2024-04-04 21:14:38 +0200

    firmware partially working

--- kinematics/lib.rs
+++ kinematics/lib.rs
@@ -1,3 +1,22 @@
 pub mod forward;
 pub mod inverse;
 pub mod model;
+pub mod motion;
+
+use tokio::sync::watch;
+
+pub enum ArmOp {
+
+}
+
+pub struct ArmTask {
+
+}
+
+impl ArmTask {
+
+}
+
+pub struct ArmHandle {
+
+}
---
+++ kinematics/motion/circular.rs
@@ -0,0 +1,12 @@
+use nalgebra::Vector3;
+
+pub struct CircularMotion {
+    center: Vector3<f64>,
+    radius: f64,
+}
+
+impl CircularMotion {
+    async fn play(&self) {
+
+    }
+}
